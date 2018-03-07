753b787e9d88f3dd961418aaea63e0ee1e592e45
marinsagovac <marin@sagovac.com>
2018-03-07 01:16:11 +0100

    Bootstrap phpunit changed path

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -12,4 +12,4 @@ error_reporting(E_ALL | E_STRICT);
  * Autoloader
  */
 require __DIR__.'./../../../../vendor/autoload.php';
-//require '../../../..///autoload.php';
+//require '../../../autoload.php';
